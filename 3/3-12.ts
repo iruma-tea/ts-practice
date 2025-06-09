72a3d6e2921cdbb857c578bda30991d6e654739b
shusuke-iruma-tea <shusuke.pana@gmail.com>
2025-06-09 16:00:08 +0900

    3-13まで学習完了

---
+++ 3/3-12.ts
@@ -0,0 +1,2 @@
+type Role = number | string; // number 型と string 型のユニオン型にRoleという名前をつける
+type EventType = "click" | "hover" | "keydown"; // リテラル型のユニオン型にEventTypeという名前をつける
---
+++ 3/3-13.ts
@@ -0,0 +1,5 @@
+{
+    type Role = number | string; // 型エイリアスを宣言
+    let firstRole: Role; // Role 型を指定
+    let lastRole: Role;  // Role 型を使い回す
+}
