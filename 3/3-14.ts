a9737fc2fcaf52f395cbebc9cdc8a8724f78241a
shusuke-iruma-tea <shusuke.pana@gmail.com>
2025-06-10 21:53:18 +0900

    オブジェクトリテラルで生成したオブジェクトを代入のサンプルを追加

---
+++ 3/3-14.ts
@@ -0,0 +1,6 @@
+type Animal = Cat | Dog;
+
+type Cat = "mike" | "dora" | "persian";
+type Dog = "shiba" | "poodle" | "pug";
+
+let pet: Animal = "shiba"; // OK
---
+++ 3/3-15.ts
@@ -0,0 +1,11 @@
+// オブジェクトリテラルで生成したオブジェクトを代入
+const person = {
+    name: "Alice",
+    age: 30,
+};
+
+// 型推論の結果
+// const person: {
+//     name: string;
+//     age: number;
+// }
