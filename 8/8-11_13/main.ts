8aef1a6e29220f8fc832c577cc8b24f60701a596
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2025-08-09 16:12:54 +0900

    is-oddのインストール/宣言ファイルの自作8-13まで学習完了

---
+++ 8/8-11_13/main.ts
@@ -0,0 +1,7 @@
+import isOdd from "is-odd";
+
+console.log(isOdd(2)); // false
+console.log(isOdd("3")); // true
+
+// モジュール 'is-odd' の宣言ファイルが見つかりませんでした。'/home/shusuke/work_ts/ts-practice/node_modules/is-odd/index.js' は暗黙的に 'any' 型になります。
+//   存在する場合は `npm i --save-dev @types/is-odd` を試すか、`declare module 'is-odd';` を含む新しい宣言 (.d.ts) ファイルを追加します
